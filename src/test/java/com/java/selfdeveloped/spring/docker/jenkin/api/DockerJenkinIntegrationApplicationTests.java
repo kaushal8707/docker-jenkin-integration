f24e2022873fde96d08caefe82c6a070d7c64fed
Kaushal Kumar <kaukumar3@publicisgroupe.net>
2023-06-18 19:37:39 +0530

    second commit

--- src/test/java/com/java/selfdeveloped/spring/docker/jenkin/api/DockerJenkinIntegrationApplicationTests.java
+++ src/test/java/com/java/selfdeveloped/spring/docker/jenkin/api/DockerJenkinIntegrationApplicationTests.java
@@ -1,13 +1,19 @@
 package com.java.selfdeveloped.spring.docker.jenkin.api;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DockerJenkinIntegrationApplicationTests {
-
+	Logger log=LoggerFactory.getLogger(DockerJenkinIntegrationApplication.class);
 	@Test
 	void contextLoads() {
+		log.info("log started");
+		assertThat("my log executing here"); 
 	}
 
 }
