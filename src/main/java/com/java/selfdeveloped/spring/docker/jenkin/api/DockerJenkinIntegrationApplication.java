202a4ad7dceb15ff26c27ca1aa5d517ac6b7faa5
Kaushal Kumar <kaukumar3@publicisgroupe.net>
2023-06-18 18:21:21 +0530

    first commit

---
+++ src/main/java/com/java/selfdeveloped/spring/docker/jenkin/api/DockerJenkinIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.java.selfdeveloped.spring.docker.jenkin.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/selfdeveloped/spring/docker/jenkin/api/DockerJenkinIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.selfdeveloped.spring.docker.jenkin.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
